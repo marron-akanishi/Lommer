f7ae67a447d891102178012c40f7d727f34ea668
Marron Akanishi <marron.general@gmail.com>
2017-09-18 00:01:54 +0900

    プロジェクト ファイルを追加します。

---
+++ Lommer/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Lommer {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Process:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Physical Memory Use(MB) :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Virtual Memory Use(MB) :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Physical Memory Max(MB) :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Virtual Memory Max(MB) :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(162, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(360, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 12);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(162, 61);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 12);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(361, 61);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 12);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(223, 84);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(137, 12);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "CPU Use(All Processes) :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(361, 84);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 12);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(9, 84);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(150, 12);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "Physical Memory Free(MB) :";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(162, 84);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 12);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "label13";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 111);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lommer";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Lommer/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Management;
+
+namespace Lommer {
+    public partial class Form1 : Form {
+        string ProgramName = "";
+        Process Target;
+        long PhysicalMax = 0;
+        long VirtualMax = 0;
+        Timer Counter = new Timer();
+        PerformanceCounter CpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        PerformanceCounter FreeRamCounter = new PerformanceCounter("Memory", "Available KBytes");
+
+        public Form1() {
+            InitializeComponent();
+            label6.Text = label7.Text = label8.Text = label9.Text = label11.Text = label13.Text = "0";
+            Counter.Interval = 1000;
+            Counter.Tick += new EventHandler(Process_Checker);
+            Counter.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Executable File(*.exe)|*.exe";
+            ofd.Title = "Please select target program.";
+            ofd.RestoreDirectory = true;
+            if(ofd.ShowDialog() == DialogResult.OK) {
+                ProgramName = ofd.FileName;
+                textBox1.Text = Path.GetFileName(ProgramName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if(ProgramName == "") {
+                MessageBox.Show("Please set target program first!");
+                return;
+            }
+            button2.Enabled = false;
+            Target = Process.Start(ProgramName);
+            Counter.Enabled = true;
+        }
+
+        private void Process_Checker(object Sender, EventArgs e) {
+            long PhyNow, VirNow;
+            try {
+                Target.Refresh();
+                PhyNow = Target.WorkingSet64 / 1024 / 1024;
+                VirNow = Target.VirtualMemorySize64 / 1024 / 1024;
+            }
+            catch {
+                Counter.Enabled = false;
+                button2.Enabled = true;
+                return;
+            }
+            int cpu = (int)CpuCounter.NextValue();
+            int ram = (int)FreeRamCounter.NextValue() / 1024;
+            if (PhyNow > PhysicalMax) PhysicalMax = PhyNow;
+            if (VirNow > VirtualMax) VirtualMax = VirNow;
+            label6.Text = PhyNow.ToString();
+            label7.Text = VirNow.ToString();
+            label8.Text = PhysicalMax.ToString();
+            label9.Text = VirtualMax.ToString();
+            label11.Text = cpu.ToString() + "%";
+            label13.Text = ram.ToString();
+        }
+    }
+}
